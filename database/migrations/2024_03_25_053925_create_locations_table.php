6485d0b522d3a7b74e19ba765c0363df86b006d8
Farhan <121376755+Grizz96@users.noreply.github.com>
2025-05-08 12:09:17 +0700

    Update 2024_03_25_053925_create_locations_table.php

--- database/migrations/2024_03_25_053925_create_locations_table.php
+++ database/migrations/2024_03_25_053925_create_locations_table.php
@@ -13,9 +13,13 @@ return new class extends Migration {
             $table->id();
             $table->decimal('latitude', 10, 8); // Kolom untuk menyimpan latitude
             $table->decimal('longitude', 11, 8); // Kolom untuk menyimpan longitude
+            $table->foreignId('shift_id') // Kolom foreign key
+                  ->constrained('shift') // Mengacu ke tabel guards
+                  ->onDelete('cascade'); // Hapus data jika parent dihapus
             $table->foreignId('guard_id') // Kolom foreign key
                   ->constrained('guards') // Mengacu ke tabel guards
                   ->onDelete('cascade'); // Hapus data jika parent dihapus
+            $table->timestamps();
         });
     }
 
