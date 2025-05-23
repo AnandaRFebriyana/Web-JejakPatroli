7ee58dc18f79c7aa616bafe4923cda33dc9afe1f
SyaifWafy <e41222174@polije.ac.id>
2025-05-23 09:13:05 +0700

    update: Tracking on map

--- resources/views/pages/location/locationtrack.blade.php
+++ resources/views/pages/location/locationtrack.blade.php
@@ -11,18 +11,42 @@
         </p>
       </div>
       <div class="flex-auto px-6 pt-4 pb-6">
-        <div class="mt-4 rounded-2xl overflow-hidden">
-          <iframe
-            width="100%"
-            height="420"
-            frameborder="0"
-            style="border:0;"
-            src="https://www.google.com/maps/embed/v1/view?key=AIzaSyC_F42ahP6FP8h-SGNFgeOvxMPd8hLO0sc&center={{ $location->latitude }},{{ $location->longitude }}&zoom=17"
-            allowfullscreen>
-          </iframe>
-        </div>
+        <div id="map" style="height: 420px; width: 100%; border-radius: 0.75rem; overflow: hidden;"></div>
       </div>
     </div>
   </div>
 </div>
+
+<!-- Google Maps JS API -->
+<script
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_F42ahP6FP8h-SGNFgeOvxMPd8hLO0sc&callback=initMap"
+  async
+  defer
+></script>
+
+<script>
+  function initMap() {
+    const center = { lat: parseFloat('{{ $location->latitude }}'), lng: parseFloat('{{ $location->longitude }}') };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 17,
+      center: center,
+    });
+
+    const patrolPathCoordinates = [
+      center,
+      { lat: center.lat + 0.0001, lng: center.lng + 0.0001 },
+      { lat: center.lat + 0.0002, lng: center.lng + 0.0002 },
+    ];
+
+    const patrolPath = new google.maps.Polyline({
+      path: patrolPathCoordinates,
+      geodesic: true,
+      strokeColor: '#0000FF',
+      strokeOpacity: 1.0,
+      strokeWeight: 4,
+    });
+
+    patrolPath.setMap(map);
+  }
+</script>
 @endsection
